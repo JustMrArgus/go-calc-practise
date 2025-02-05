f47611836b0a78b305e5c730c8e9cb0a8c916100
JustMrArgus <argusprogramming@protonmail.com>
2025-02-05 13:38:55 +0000

    Add stuff

--- main.go
+++ main.go
@@ -21,13 +21,60 @@ func divide(x, y int) int {
 	return x / y
 }
 
-func show_pi() float32 {
-	return math.Pi
+func square(x int) float64 {
+	if x < 0 {
+		return -1
+	}
+	return math.Sqrt(float64(x))
 }
 
 func main() {
-	fmt.Println("Hello User!")
-	fmt.Println(add(1, 5))
-	fmt.Println(show_pi())
-	fmt.Println("Goodbye User!")
+	fmt.Println("Welcome to GoCalc!")
+	for {
+		fmt.Println("Choose your option:")
+		fmt.Println("1 - Add")
+		fmt.Println("2 - Substract")
+		fmt.Println("3 - Multiplie")
+		fmt.Println("4 - Divide")
+		fmt.Println("5 - Square root")
+		fmt.Println("")
+
+		var option int
+		fmt.Println("Enter number of option: ")
+		fmt.Scan(&option)
+
+		var num1 int
+		var num2 int
+		fmt.Println("Enter first number: ")
+		fmt.Scan(&num1)
+		fmt.Println("Enter second number: ")
+		fmt.Scan(&num2)
+
+		switch option {
+		case 1:
+			fmt.Println("")
+			fmt.Println(add(num1, num2))
+			fmt.Println("")
+		case 2:
+			fmt.Println("")
+			fmt.Println(substract(num1, num2))
+			fmt.Println("")
+		case 3:
+			fmt.Println("")
+			fmt.Println(multiplie(num1, num2))
+			fmt.Println("")
+		case 4:
+			fmt.Println("")
+			fmt.Println(divide(num1, num2))
+			fmt.Println("")
+		case 5:
+			fmt.Println("")
+			fmt.Println(square(num1))
+			fmt.Println("")
+		default:
+			fmt.Println("")
+			fmt.Println("Invalid number of option, choose another one")
+			fmt.Println("")
+		}
+	}
 }
